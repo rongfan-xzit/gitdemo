d01b7ab5eed898f4acb05679454072ab43155c7a
rong <895657843@qq.com>
2019-09-27 17:04:39 +0800

    小袁提交新项目

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git 小院");
+        System.out.println("Hell Git 小院");
         System.out.println("我是小袁");
     }
 }
